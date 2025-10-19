2151e58d202879a4f4b7f90e29b2dd0b855635a6
gzl-shuai <1558507848@qq.com>
2025-10-19 13:18:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-# main.py
-text = input()
+# charcount.py
+def char_count(text):
+    letters = digits = spaces = others = 0
+    
+    for char in text:
+        if char.isalpha():
+            letters += 1
+        elif char.isdigit():
+            digits += 1
+        elif char.isspace():
+            spaces += 1
+        else:
+            others += 1
+            
+    return letters, digits, spaces, others
 
-letters = 0
-digits = 0
-spaces = 0
-others = 0
-
-for char in text:
-    if char.isalpha():
-        letters += 1
-    elif char.isdigit():
-        digits += 1
-    elif char.isspace():
-        spaces += 1
-    else:
-        others += 1
-
-print(f"英文字符: {letters}")
-print(f"数字: {digits}")
-print(f"空格: {spaces}")
-print(f"其他字符: {others}")
+if __name__ == "__main__":
+    text = input()
+    letters, digits, spaces, others = char_count(text)
+    print(f"英文字符: {letters}")
+    print(f"数字: {digits}")
+    print(f"空格: {spaces}")
+    print(f"其他字符: {others}")
