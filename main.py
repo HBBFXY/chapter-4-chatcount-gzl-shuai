6343326ab1f3553dd78eec9e2a1f576d78e1c69c
gzl-shuai <1558507848@qq.com>
2025-10-19 12:46:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-
-# 从键盘输入一行字符
 text = input("请输入一行字符：")
 
-# 初始化计数器
-letters = 0  # 英文字符
-digits = 0   # 数字
-spaces = 0   # 空格
-others = 0   # 其他字符
+letters = digits = spaces = others = 0
 
-# 遍历每个字符并进行分类统计
 for char in text:
-    if char.isalpha():  # 判断是否为英文字母
+    if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
         letters += 1
-    elif char.isdigit():  # 判断是否为数字
+    elif '0' <= char <= '9':
         digits += 1
-    elif char.isspace():  # 判断是否为空格
+    elif char == ' ':
         spaces += 1
-    else:  # 其他字符
+    else:
         others += 1
 
-# 输出统计结果
-print("英文字符: %d" % letters)
-print("数字: %d" % digits)
-print("空格: %d" % spaces)
-print("其他字符: %d" % others)
+print("英文字符:", letters)
+print("数字:", digits)
+print("空格:", spaces)
+print("其他字符:", others)
